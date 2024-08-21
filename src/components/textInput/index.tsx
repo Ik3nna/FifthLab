9dfb7ca855bcddada0b6bfe73f210d68e22d7d35
ik3nna <iamiykendukwu@gmail.com>
2024-08-21 13:05:36 +0100

    feat: Filter and Navigation UI implementation

--- src/components/textInput/index.tsx
+++ src/components/textInput/index.tsx
@@ -1,18 +1,24 @@
 import React, { ChangeEvent } from 'react'
 import styles from "./index.module.css"
+import homeStyles from "@/pages/home/index.module.css"
 
 interface InputProps {
   type: string,
   placeholder?: string,
   icon: React.ReactNode,
   value: string,
-  onChange: (e: ChangeEvent<HTMLInputElement>)=> void
+  onChange: (e: ChangeEvent<HTMLInputElement>)=> void,
+  className?: any,
   [props: string]: any
 }
 
-const TextInput = ({ type, placeholder, value, onChange, icon, ...props }: InputProps) => {
+const TextInput = ({ type, placeholder, value, onChange, className, icon, ...props }: InputProps) => {
   return (
-    <div className={styles.input_container}>
+    <div 
+      className={`
+        ${styles.input_container}
+        ${homeStyles[className]}
+      `}>
       {icon && <div>{icon}</div>}
 
       <input 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./index.module.css"
 import TextInput from '@/components/textInput'
 import { AiOutlineSearch } from "react-icons/ai";
 import { IoIosPeople } from "react-icons/io";
 import { FaMale, FaFemale } from "react-icons/fa";
 import Cards from '@/components/cards';
-
+import { Country } from "country-state-city";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import Switch from "react-switch";
+import { BiCloudDownload } from "react-icons/bi";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 const usersData = [
     {
@@ -26,6 +31,12 @@ const usersData = [
 ] 
 
 const Home: React.FC = () => {
+    const [checked, setChecked] = useState(true);
+
+    const countryData = Country.getAllCountries().map((item) => item.name).map(country => ({
+        value: country,
+        label: country
+    }));
 
     return (
         <section className={styles.container}>
@@ -68,7 +79,63 @@ const Home: React.FC = () => {
             </article>
 
             <article className={styles.user_details}>
+                <div className={styles.filter}>
+                    <div>
+                        <div>All Users</div>
+                        <div>Filter by</div>
+                    </div>
 
+                    <div className={styles.inputs_container}>
+                        <TextInput 
+                            value=''
+                            onChange={()=>{}}
+                            type='text'
+                            placeholder='Find in list'
+                            className="input"
+                            icon={
+                                <AiOutlineSearch 
+                                    color='var(--inputPlaceholderHex)'
+                                    size={20} 
+                                />
+                            }
+                        />
+
+                        <Dropdown 
+                            options={countryData} 
+                            placeholder="country"
+                            value={countryData.find((item)=>item.value==="Nigeria")}
+                        />
+
+                        <div className={styles.switch_container}>
+                            <Switch 
+                                checked={checked}
+                                checkedIcon={false}
+                                onColor="#30BBB5"
+                                onHandleColor='#30BBB5'
+                                onChange={()=>{}}
+                            />
+
+                            <span>Show Country</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.navigation}>
+                    <a href="https://randomuser.me/api/?format=csv" className={styles.download}>
+                        <BiCloudDownload size={30} color='var(--whiteHex)' />
+                        <span>Download Results</span>
+                    </a>
+
+                    <div>
+                        <div className={styles.previous}>
+                            <MdKeyboardArrowLeft color='var(--nextHex)' size={24} />
+                        </div>
+
+                        <div className={styles.next}>
+                            <MdKeyboardArrowRight color='var(--whiteHex)' size={24} />
+                        </div>
+                    </div>
+                </div>
             </article>
         </section>
     )
